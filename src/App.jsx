cd3c2ef71041511db74337525dc26727e5a96915
JHS <118969653+ARISTAHS@users.noreply.github.com>
2024-12-24 17:58:51 +0900

    app 컴포넌트 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createGlobalStyle } from 'styled-components';
+import reset from "styled-reset";
+
+//전역 css + 리셋
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+   * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    vertical-align: top;
+  }
+
+  body {
+    font-family: 'Arial', sans-serif;
+    background-color: #f5f5f5;
+    color: #333;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  a:focus {
+  outline: 2px dashed #fff;
+  outline-offset: 4px;
+  }
+  
+  input[type=text] , input[type=email] ,input[type=password] {
+  outline: none;
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid #d9e0e6;
+  }
+
+  input:focus{outline: none;}
+
+  button{
+    cursor: pointer;
+    border: none;
+    background-color: #fff;
+  }
+
+  button:focus{border: none;}
+
+  .hidden{
+    display: none;
+  }
+
+`;
+
+//라우터 설정
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page</div>, // 임시로 Home Page 컴포넌트
+  },
+  {
+    path: "/about",
+    element: <div>About Page</div>, // 임시로 About Page 컴포넌트
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyle /> {/* 전역 스타일 적용 */}
+      <RouterProvider router={router} /> {/* RouterProvider로 라우터 설정 */}
     </>
   )
 }

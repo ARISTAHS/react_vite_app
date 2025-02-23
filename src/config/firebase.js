bcea81319ed793d94796690989d1c99361ee0136
JHS <118969653+ARISTAHS@users.noreply.github.com>
2025-02-23 20:43:40 +0900

    firebase api 키, 재작업

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import config from "./config";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAGTc9pLHESR046iH04QxM6IqB1F4qTesM",
-  authDomain: "fencingweb-a6cc5.firebaseapp.com",
-  projectId: "fencingweb-a6cc5",
-  storageBucket: "fencingweb-a6cc5.firebasestorage.app",
-  messagingSenderId: "280938600226",
-  appId: "1:280938600226:web:fb64f6e15882df9cd3377b",
-  measurementId: "G-GCKJ99QG50"
+  apiKey: config.FIREBASE_API_KEY,
+  authDomain: config.FIREBASE_AUTH_DOMAIN,
+  projectId: config.FIREBASE_PROJECT_ID,
+  storageBucket: config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: config.FIREBASE_APP_ID,
+  measurementId: config.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);

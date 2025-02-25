ae050e931e697e8abef0c2e2b5eab2d538396d95
JHS <118969653+ARISTAHS@users.noreply.github.com>
2025-02-25 11:53:35 +0900

    firebase 로그인 요청 및 확인 로직 구현

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
+
 
 const LoginWrap = styled.div`
   .form_wrap{
@@ -22,22 +27,60 @@ const LoginWrap = styled.div`
 
 
 export default function Login(){
+
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault(); // 기본 동작 막기
+
+    if (!username || !password) {
+      navigate("/error"); // 에러 페이지로 이동
+      return;
+    }
+
+    try {
+      // Firebase 로그인 요청
+      await signInWithEmailAndPassword(auth, username, password);
+      navigate("/main"); // 로그인 성공 시 메인 페이지로 이동
+    } catch (error) {
+      console.error("로그인 오류:", error);
+      navigate("/error"); // 로그인 실패 시 에러 페이지로 이동
+    }
+  };
+
   return(
     <LoginWrap>
       <div className="form_wrap">
         <div className="login_box">
           <h2>로그인</h2>
-          <form name="loginForm">
+          <form name="loginForm" onSubmit={handleLogin}>
             <div className="id_input">
-              <input autoComplete="username" maxLength={15} name="username" type="text" />
+              <input
+                autoComplete="username"
+                maxLength={30}
+                name="username"
+                type="email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
               <label htmlFor="username">이메일</label>
             </div>
             <div className="ps_input">
-              <input autoComplete="password" maxLength={20} name="password" type="text" />
+              <input
+                autoComplete="password"
+                maxLength={30}
+                name="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <label htmlFor="password">비밀번호</label>
             </div>
           </form>
-          <button className="login_btn">로그인</button>
+          <button type="submit" className="login_btn">로그인</button>
+
           <ul className="menu_list">
             <li>
               <Link to="/main">회원가입</Link>
@@ -49,6 +92,7 @@ export default function Login(){
               <Link to="/main">비밀번호찾기</Link>
             </li>
           </ul>
+
         </div>
       </div>
     </LoginWrap>

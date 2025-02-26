02854193d7c93b2bb31c87c99ea407572295f021
JHS <118969653+ARISTAHS@users.noreply.github.com>
2025-02-27 01:02:51 +0900

    Gnb nav 영역 수정 및 로그인 페이지 이동 체크

--- frontend/src/components/common/gnb.jsx
+++ frontend/src/components/common/gnb.jsx
@@ -1,29 +1,54 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import LoginPage from "../../pages/login";
 
 const Nav = styled.nav`
-  background-color: #333;
   padding: 1rem;
-`;
-const Ul = styled.ul`
-  list-style: none;
   display: flex;
-  gap: 20px;
-`;
-const Li = styled.li`
-  font-size: 1.2rem;
-  color: #fff;
+  justify-content: center;
+  align-items: center;
+  gap: 10rem;
 
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
+  .sub_box{
+    .sub_list{
+      list-style: none;
+      display: flex;
+      gap: 20px;
 
-  &:hover {
-    color: #ff6347;
+      li{
+        font-size: 1.2rem;
+        color: #000;
+        a{
+          text-decoration: none;
+          color: inherit;
+        }
+        &:hover {
+          color: #ff6347;
+        }
+      }
+    }
+  }
+  .etc_box{
+    .etc_list{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 20px;
+
+      li{
+        /* width: 60px; */
+        color: #666;
+        font-size: 0.9rem;
+
+        a{
+          display: block;
+          width: 100%;
+          
+        }
+      }
+    }
   }
 `;
+
 
 export default function Gnb() {
 
@@ -36,14 +61,27 @@ export default function Gnb() {
 
   return (
     <Nav>
-      <Ul>
-        {/* 반복되는 li 태그를 map 함수로 수정 */}
-        {navLi.map((link)=>(
-          <Li key={link.path}>
-            <Link to={link.path}>{link.label}</Link>
-          </Li>
-        ))}
-      </Ul>
+      <div className="sub_box">
+        <ul className="sub_list">
+          {/* 반복되는 li 태그를 map 함수로 수정 */}
+          {navLi.map((link)=>(
+            <li key={link.path}>
+              <Link to={link.path}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      
+      <div className="etc_box">
+        <ul className="etc_list">
+          <li>
+            <Link to="/login">로그인</Link>
+          </li>
+          <li>
+            <Link to="/login">회원가입</Link>
+          </li>
+        </ul>
+      </div>
       
     </Nav>
   );

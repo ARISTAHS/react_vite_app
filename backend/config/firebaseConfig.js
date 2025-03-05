6e316434aed848d9a22fbf5a479c5121e9cf3a04
JHS <118969653+ARISTAHS@users.noreply.github.com>
2025-03-05 21:54:36 +0900

    로그인 오류 문제 해결 중

---
+++ backend/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+const { initializeApp } = require("firebase/app");
+const { getAuth } = require("firebase/auth");
+const config = require("../../frontend/"); // 환경변수 파일 불러오기
+
+// Firebase 앱 초기화
+const firebaseConfig = {
+  apiKey: config.FIREBASE_API_KEY,
+  authDomain: config.FIREBASE_AUTH_DOMAIN,
+  projectId: config.FIREBASE_PROJECT_ID,
+  storageBucket: config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: config.FIREBASE_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+module.exports = { firebaseApp, auth };

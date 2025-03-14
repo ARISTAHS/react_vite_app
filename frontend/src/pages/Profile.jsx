a113e0738fd46fa243ca48be392d973c4589bce7
JHS <118969653+ARISTAHS@users.noreply.github.com>
2025-03-14 19:22:17 +0900

    개인정보 수정 form

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+
+const ProfileWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
 export default function ProfilePage(){
   return(
-    <>
-      
-    </>
+    <ProfileWrap>
+      <h2 className="inner_tit">개인정보 수정</h2>
+      <div className="form_wrap">
+        <form name="profile_form" method="post" action="">
+          <dl>
+            <dt>
+              <label htmlFor="name">이름</label>
+            </dt>
+            <dd>
+              <input type="text" placeholder="이름"/>
+            </dd>
+          </dl>
+        </form>
+      </div>
+    </ProfileWrap>
   );
 }

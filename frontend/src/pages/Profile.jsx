5924f95be913476f4acb533933953533fadaaa00
JHS <118969653+ARISTAHS@users.noreply.github.com>
2025-03-17 19:13:40 +0900

    map 함수의 매개변수 객체로 수정 및 오류 수정

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -32,22 +32,14 @@ export default function ProfilePage(){
     <ProfileWrap>
       <h2 className="inner_tit">개인정보 수정</h2>
       <div className="form_wrap">
-        <form name="profile_form" method="post" action="">
-          {/* <dl>
-            <dt>
-              <label htmlFor="name">이름</label>
-            </dt>
-            <dd>
-              <input type="text" placeholder="이름"/>
-            </dd>
-          </dl> */}
-          {formData.map((Data)=>(
-            <dl key={Data.id}>
+        <form name="profile_form" method="post">
+          {formData.map(({id, label, type, placeholder})=>(
+            <dl key={id}>
               <dt>
-                <label htmlFor={Data.label}>{Data.label}</label>
+                <label htmlFor={id}>{label}</label>
               </dt>
               <dd>
-                <input type={Data.type} id={Data.id} placeholder={Data.placeholder} />
+                <input type={type} id={id} placeholder={placeholder} />
               </dd>
             </dl>
           ))}

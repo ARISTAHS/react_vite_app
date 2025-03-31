e40c3e6de04136b2213ad000fdeaaaa26e65d352
JHS <118969653+ARISTAHS@users.noreply.github.com>
2025-03-31 19:07:29 +0900

    로그아웃 시 recoil 상태 초기화 추가

--- frontend/src/components/common/LogoutButton.jsx
+++ frontend/src/components/common/LogoutButton.jsx
@@ -1,13 +1,18 @@
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../config/firebase";
+import { useSetRecoilState } from "recoil";     
+import { userState } from "../../recoil/userAtom";
+// recoil에 로그인 정보가 남아있을 가능성. 
 
 export default function LogoutButton(){
   const navigate = useNavigate();
+  const setUser = useSetRecoilState(userState);
 
   const handleLogout = async () =>{
     try{
-      await signOut(auth);
+      await signOut(auth);  // firebase 로그아웃
+      setUser(null);        // recoil 상태 초기화
       navigate('/main');
     } catch(error){
       console.error("로그아웃 실패:",error);
